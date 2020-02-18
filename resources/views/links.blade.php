509c013651944474d98f71867777a44f3c14a898
Jonathan <jg.pradi@gmail.com>
2020-02-18 20:27:28 -0300

    cambio

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,6 +1,18 @@
-<a href="{{ route('socialLogin.index', 'facebook') }}" class="btn btn-block btn-facebook">
-   <i class="fa fa-facebook"></i> Ingresar con Facebook
-</a>
-<a href="{{ route('socialLogin.index', 'google') }}" class="btn btn-block btn-google">
-   <i class="fa fa-google"></i> Ingresar con Google
-</a>
+@if (config('dvs-socialite.socialsNetworks')->contains('facebook'))
+   <a href="{{ route('socialLogin.index', 'facebook') }}" class="btn btn-block btn-facebook">
+      <i class="fa fa-facebook"></i> Ingresar con Facebook
+   </a>
+@endif
+
+@if (config('dvs-socialite.socialsNetworks')->conatins('google'))
+   <a href="{{ route('socialLogin.index', 'google') }}" class="btn btn-block btn-google">
+      <i class="fa fa-google"></i> Ingresar con Google
+   </a>
+@endif
+
+@if (config('dvs-socialite.socialsNetworks')->conatins('twitter'))
+   <a href="{{ route('socialLogin.index', 'twitter') }}" class="btn btn-block btn-twitter">
+      <i class="fa fa-twitter"></i> Ingresar con Twitter
+   </a>
+@endif
+
